30ff80a89af5ff1195ba8d656dfb953bd7935ad7
Jonathan Lee <jonathanlee882@gmail.com>
2023-05-22 14:16:39 +1000

    added Landing Page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
 import Nav from "./components/Nav";
+import Landing from './components/Landing';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-library.add(faBars)
+import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars, faShoppingCart, faTimes)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
