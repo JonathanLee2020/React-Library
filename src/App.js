15c26201e2a2f409174da99f6a4e5a2911dbb4a3
Jonathan Lee <jonathanlee882@gmail.com>
2023-05-23 14:54:01 +1000

    added router funcitonality

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Featured from './components/Featured';
 import Discounted from './components/Discounted';
 import Explore from './components/Explore';
 import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Landing />
+        <Highlights />
+        <Featured />
+        <Discounted />
+        <Explore />
+        <Footer />
+      </div>
+      </Router>
   )
 }
 
