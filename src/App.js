b8426fd26ed263b86b70b7958d58e594d3b66ad4
Jonathan Lee <jonathanlee882@gmail.com>
2023-05-23 15:46:51 +1000

    added books page and currently passing in data from books through an arrow function

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Discounted from './components/Discounted';
 import Explore from './components/Explore';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from './pages/Home';
+import Books from "./pages/Books"
 
 
 function App() {
@@ -14,11 +16,8 @@ function App() {
     <Router>
       <div className="App">
         <Nav />
-        <Landing />
-        <Highlights />
-        <Featured />
-        <Discounted />
-        <Explore />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={<Books  books={books}/>} />
         <Footer />
       </div>
       </Router>
